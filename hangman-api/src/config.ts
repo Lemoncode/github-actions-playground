f81ee4bae25fcaff3e48a4382a378db4a3f5d9bc
jaimesalas <jaime.salas@lemoncode.net>
2022-10-09 18:37:54 +0200

    started back solution

---
+++ hangman-api/src/config.ts
@@ -0,0 +1,18 @@
+import { config } from 'dotenv';
+
+config({
+  path: '.env',
+});
+
+export default {
+  database: {
+    isActive: process.env.DATA_BASE_ACTIVE,
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: +process.env.DATABASE_PORT!,
+    database: process.env.DATABASE_NAME,
+    poolMin: +process.env.DATABASE_POOL_MIN!,
+    poolMax: +process.env.DATABASE_POOL_MAX!,
+  },
+};
